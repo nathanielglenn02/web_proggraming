6661fe67fa242e05f4a0ae75ed83952011d91cf0
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-05-10 06:49:16 +0700

    latihan

---
+++ quiz2/quizpage1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="quizpage2.php" method="post">
+        <input type="text" name='user[]' id='userl' values="user1">
+        <input type="text" name='user[]' id='user2' value="user2">
+        <input type="text" name='user[]' id='user3'  value="user3">
+    </form>
+
+
+</body>
+
+</html>
